9e93f9c4957d46545f8e961aed354c51dfcfa940
nimendra <nimendraamiththa1@gmail.com>
2024-07-31 11:54:05 +0530

    removed the slash in the origin link

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors(
   {
-    origin: ["https://image-testing-frontend.vercel.app/"],
+    origin: ["https://image-testing-frontend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }
