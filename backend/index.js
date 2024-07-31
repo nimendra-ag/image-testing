18e0105ee22f812647520135e33a75215b4176eb
nimendra <nimendraamiththa1@gmail.com>
2024-07-31 10:45:18 +0530

    updated 1

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors(
   {
-    origin: ["image-testing-xi.vercel.app"],
+    origin: ["https://image-testing-xi.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true
   }
