a8d5e73966edb371b4376cd24178d6febfad617c
nimendra <nimendraamiththa1@gmail.com>
2024-07-31 10:27:16 +0530

    added vercel.json file. added some code in index.js regarding cors.

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors(
+  {
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 
 // Cloudinary configuration
 cloudinary.config({
